b010349c32c9548c836829679aaf6512b5abe402
Alden Liu <aldenliu98@berkeley.edu>
2019-01-31 16:18:18 -0800

    hi albert

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,12 +1,48 @@
 import React from "react";
-// import styled from "styled-components";
+import { 
+  MDBNavbar, 
+  MDBNavbarBrand, 
+  MDBNavbarNav, 
+  MDBNavItem,
+  MDBNavLink,
+  MDBIcon,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+} from "mdbreact";
+import styled from "styled-components";
 import { Redirect } from "react-router-dom";
 import {getCookie, checkCookie} from "../utils/util";
 import * as consts from "../utils/constants"
+import Background from "../assets/images/working.jpg";
 
 import Login from "./login";
 import SignUp from "./signup";
 
+const Header_Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+
+const Container = styled.div`
+  background-image: url(${Background});
+  background-size: cover;
+`
+
+const Header_One = styled.p`
+  font-size: 100px;
+  color: white;
+`
+
+const Header_Liner = styled.p`
+  font-size: 100px;
+  font-weight: bold;
+  color: white;
+`
 
 class Welcome extends React.Component {
   state = {
@@ -47,16 +83,46 @@ class Welcome extends React.Component {
       return <Redirect to={{pathname: '/home', state: {token: getCookie(consts.ACCESS_TOKEN_KEY)}}}/>;
     } else {
       return (
-        <div>
-          <button onClick={this.showLogin}>
-            Login
-          </button>
-          <Login show={this.state.showLogin} handleClose={this.hideLogin}/>
-          <button onClick={this.showSignUp}>
-            SignUp
-          </button>
-          <SignUp show={this.state.showSignUp} handleClose={this.hideSignUp}/>
-        </div>
+        <Container>
+
+          <MDBNavbar style={{backgroundColor: "rgba(0, 0, 0, 0.5)"}} dark expand="md">
+            <MDBNavbarBrand>
+              <strong className="white-text">OneLiner</strong>
+            </MDBNavbarBrand>
+              <MDBNavbarNav left>
+              </MDBNavbarNav>
+              <MDBNavbarNav right>
+                <MDBNavItem className="px-3">
+                  <MDBNavLink className="waves-effect waves-light" to="">
+                    <MDBIcon icon="cog" className="mr-1" /> Settings
+                  </MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem className="px-3">
+                  <MDBDropdown>
+                    <MDBDropdownToggle nav caret>
+                      <MDBIcon icon="user" className="mr-1" />Profile
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu className="dropdown-default" right>
+                      <MDBDropdownItem onClick={this.showLogin}>Login</MDBDropdownItem>
+                      <MDBDropdownItem onClick={this.showSignUp}>Sign Up</MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBNavItem>
+              </MDBNavbarNav>
+          </MDBNavbar>
+
+          <Header_Container>
+
+            <Header_One>One</Header_One>
+            <Header_Liner>Liner</Header_Liner>
+
+            <Login show={this.state.showLogin} handleClose={this.hideLogin}/>
+            <SignUp show={this.state.showSignUp} handleClose={this.hideSignUp}/>
+
+          </Header_Container>
+        
+          
+        </Container>
       );
     }
     
